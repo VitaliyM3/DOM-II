d6fa615d31ec958f1f5cd95f215edda6159877ab
Vitaliy Mikhaylyyk <vitaliymikhaylyuk@gmail.com>
2019-11-01 21:39:24 -0400

    4 event listeners done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // Your code goes here
+//1. funbus transition
+let funBus = document.querySelector('img');
+funBus.addEventListener('mouseenter', () =>  {
+    funBus.style.transform="scale(0.8)";
+    funBus.style.transition="all 0.4s";
+});
+
+funBus.addEventListener('mouseleave', () => {
+    funBus.style.transform="scale(1)";
+    funBus.style.transition="all 0.3s";
+});
+
+//2. 
+let title = document.querySelector(".logo-heading");
+title.addEventListener('click', ()=> {
+    title.style.color= "blue";
+});
+
+//3.
+let sizeChange = document.querySelector(".intro h2");
+sizeChange.addEventListener('auxclick', ()=> {
+    sizeChange.style.fontSize= "2rem";
+});
+
+//4.
+
+//5.
+
+//6.
+
+//7.
+
+//8.
+
+//9.
+
+//10.
